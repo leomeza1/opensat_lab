f71ec8cd668c7818275b51058fde5d4fb155a101
Leo Meza <Leo.E.Meza@gmail.com>
2024-11-22 15:34:47 -0700

    Adding rtems-source-builder and the rtems source repositories as submodules. Also, adding a config.ini file for use with waf when build the BSP and added a build_tool_suite.sh script.

---
+++ rtems/build_tool_suite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "This script will attempt to build"
+echo "the RTEMS tool suite for the ARM"
+echo "beagleboneblack BSP."
+
+#./source-builder/sb-check
+#cd ./src/rsb/rtems
+#../source-builder/sb-set-builder --prefix=../../../rtems6 6/rtems-arm
+
+
